4e683ea0de1c60fb0176e213c3452ffd2e703708
smoothbear <kjbin0420@naver.com>
2021-06-03 17:34:36 +0900

    :bug: :: Edit desc

--- Formula/pclt.rb
+++ Formula/pclt.rb
@@ -1,5 +1,5 @@
 class Pclt < Formula
-    desc "Get Human Readable file size information. - CLI"
+    desc "For creating projects. - CLI"
     homepage "https://github.com/smoothbear/pclt"
     url "https://github.com/smoothbear/pclt/releases/download/1.0.2/pclt-mac.tar.gz"
     sha256 "4a74f768a99fa3d4b4526263e6a248d01f0adb8a813b7fff6605bb6b51777e33"
--- Formula/xmoji.rb
+++ Formula/xmoji.rb
@@ -1,5 +1,5 @@
 class Xmoji < Formula
-    desc "Get Human Readable file size information. - CLI"
+    desc "More easily for writing commit messages. - CLI"
     homepage "https://github.com/team-xquare/xmoji"
     url "https://github.com/team-xquare/xmoji/releases/download/1.1.3/xmoji.tar.gz"
     sha256 "4937292d8f03ef2771bdbca56625cdcec11cd0856a2516f929298bacc06883ae"
